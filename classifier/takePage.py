9fd3ac747b51122849a189d0499eda0b5c77d5c1
Italo Soares <irs@cin.ufpe.br>
2021-03-03 17:38:02 -0300

    Adicionando páginas positivas

--- classifier/takePage.py
+++ classifier/takePage.py
@@ -5,19 +5,11 @@ def downloadPage(url, x):
   headers = {'user-agent': 'Italo'};
   r = rq.get(url, headers=headers);
   page = BeautifulSoup(r.text, "html.parser");
-  with open("steam/negativePages/page"+str(x)+".html", "w") as file:
+  with open("steam/positivePages/page"+str(x)+".html", "w") as file:
       file.write(str(page));
       
-urls = ["https://store.steampowered.com/communityrecommendations/",
-"https://store.steampowered.com/curators/",
-"https://store.steampowered.com/genre/Early%20Access/",
-"https://store.steampowered.com/tags/pt-br/Aventura/",
-"https://store.steampowered.com/tags/pt-br/Casual/",
-"https://store.steampowered.com/tags/pt-br/A%C3%A7%C3%A3o/",
-"https://store.steampowered.com/tags/pt-br/Corrida/",
-"https://store.steampowered.com/points/",
-"https://store.steampowered.com/news/",
-"https://store.steampowered.com/labs/"];
+urls = ["https://store.steampowered.com/app/367520/Hollow_Knight/",
+"https://store.steampowered.com/app/762840/Maquette/","https://store.steampowered.com/app/1361320/The_Room_4_Old_Sins/","https://store.steampowered.com/app/860510/Little_Nightmares_II/","https://store.steampowered.com/app/945360/Among_Us/","https://store.steampowered.com/app/105600/Terraria/","https://store.steampowered.com/app/1215270/Down_the_Rabbit_Hole/","https://store.steampowered.com/app/322500/SUPERHOT/","https://store.steampowered.com/app/262060/Darkest_Dungeon/","https://store.steampowered.com/app/1097150/Fall_Guys_Ultimate_Knockout/"];
 x=1;
 for url in urls:
   downloadPage(url, x)
