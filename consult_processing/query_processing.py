a45ab62253ce3925b6f3ec0cc02375e897880491
Italo Soares <irs@cin.ufpe.br>
2021-04-27 14:21:55 -0300

    Adicionando dados faltantes no slide

--- consult_processing/query_processing.py
+++ consult_processing/query_processing.py
@@ -2,9 +2,63 @@ from nltk.corpus import stopwords
 import re
 import unidecode
 import os
+import operator
 import json
 from math import log
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+class GeneralQuery:
+    def __init__(self, queryString, description, OS, ram, storage, price, useTfIdf = True):
+        self.queryString = queryString
+        self.description = description
+        self.OS = OS
+        self.ram = ram
+        self.storage = storage
+        self.price = price
+        self.documents = os.path.abspath('../inverted_index/db/')
+        self.twoTermsPath = os.path.abspath('../inverted_index/twoTerms.json')
+        self.invIndexPath = os.path.abspath('../inverted_index/frequency.json')
+        self.fieldQuery = QueryProcessor(invIndexPath=self.twoTermsPath, documentsPath=self.documents)
+        self.wordQuery = QueryProcessor(invIndexPath=self.invIndexPath, documentsPath=self.documents)
+        self.useTfIdf = useTfIdf
+
+    def processQuery(self):
+        gameList = []
+        if(self.queryString != ''):
+            games = self.wordQuery.query(self.queryString, useTfIdf = self.useTfIdf)
+            gameList.append(games)
+        if(self.description != ''):
+            games = self.fieldQuery.query(self.description, useTfIdf=self.useTfIdf, field="description")
+            gameList.append(games)
+        if(self.storage != ''):
+            games = self.fieldQuery.query(self.storage, useTfIdf=self.useTfIdf, field="storage")
+            gameList.append(games)
+        if(self.OS != ''):
+            games = self.fieldQuery.query(self.OS, useTfIdf=self.useTfIdf, field="os")
+            gameList.append(games)
+        if(self.price != ''):
+            games = self.fieldQuery.query(self.price, useTfIdf=self.useTfIdf, field="price")
+            gameList.append(games)
+        if(self.ram != ''):
+            games = self.fieldQuery.query(self.ram, useTfIdf=self.useTfIdf, field="ram")
+            gameList.append(games)
+        
+        gamesHash = {}
+        #take average of the tfidf of each game returned in the query
+        for i in range(len(gameList)): 
+            for game in gameList[i]:
+                if not str(game[0]) in gamesHash:
+                    gamesHash[str(game[0])] = float(game[1])
+                else:
+                    gamesHash[str(game[0])] += float(game[1])
+    
+        for name in gamesHash:
+            gamesHash[name] /= 6
+
+        #sort by highest tfidf average
+        sorted_games = sorted(gamesHash.items(), key=operator.itemgetter(1), reverse=True)
+
+        return sorted_games
 
 class QueryProcessor():
     def __init__(self, invIndexPath, documentsPath):
@@ -96,7 +150,7 @@ class QueryProcessor():
         termIDF = {}
         for term in self.invIndex.keys():
             numDocsByTerm = len(self.invIndex[term])
-            print(numDocsByTerm)
+            #print(numDocsByTerm)
             idf = log(self.numDocs/numDocsByTerm)
             termIDF[term] = idf
         self.termIDF = termIDF
@@ -133,7 +187,89 @@ class QueryProcessor():
         lengthDoc = {}
         for fileName in os.listdir(self.documentsPath):
             filePath = self.documentsPath + '/' + fileName
-            print(filePath)
+            #print(filePath)
             with open(filePath, 'r') as fp:
                 lengthDoc[int(fileName)] = len(fp.read().split(' '))
         self.lengthDoc = lengthDoc
+
+# %%
+invIndexPath = os.path.abspath('../inverted_index/frequency.json')
+twoTermsPath = os.path.abspath('../inverted_index/twoTerms.json')
+documents = os.path.abspath('../inverted_index/db/')
+
+# %%
+qp = QueryProcessor(invIndexPath=invIndexPath, documentsPath=documents)
+
+# %%
+rankingTFIDF = dict(qp.query('dark souls', useTfIdf=True))
+rankingTFIDF
+
+# %%
+ranking = dict(qp.query('dark souls', useTfIdf=False))
+ranking
+
+# %%
+numDocs = len(list(ranking.keys())+list(rankingTFIDF.keys()))
+
+# %%
+def getSumSquareDist(r1, r2):
+    result = 0
+    docs = list(r1.keys())+list(r2.keys())
+    for doc in docs:
+        squareDistance = (r1.get(doc, 0)-r2.get(doc,0))**2
+        result += squareDistance
+    return result
+
+# %%
+sumSquareDistance = getSumSquareDist(rankingTFIDF, ranking)
+
+# %%
+def spearmanCorrelation(sumSquareDist, k):
+    num = 6*sumSquareDist
+    den = k*(k**2-1)
+    return 1-(num/den)
+
+# %%
+spearmanCorrelation(sumSquareDistance, k = numDocs)
+
+# %%
+def comparissonSpearmanCorrelation(queries, numDocs):
+    for q in queries:
+        ranking = dict(qp.query(q))
+        rankingTFIDF = dict(qp.query(q, useTfIdf= True))
+        sumSquareDistance = getSumSquareDist(rankingTFIDF, ranking)
+        sc = spearmanCorrelation(sumSquareDistance, k = numDocs)
+        print("Query #(" + q + "):")
+        #print("Sum Square Distance = %.2f"%(sumSquareDistance))
+        print("Spearman = %f"%(sc))
+        #print(sc)
+
+# %%
+comparissonSpearmanCorrelation(['"dorfromantik"', "hollow knight", "maquette", "little nightmares II", "windows 2gb ram"], numDocs)
+
+# %%
+a = dict(qp.query('windows 2gb ram', useTfIdf=False))
+b = dict(qp.query('windows 2gb ram', useTfIdf=True))
+
+# %%
+getSumSquareDist(a, b)
+
+# %%
+b
+
+# %%
+queryTFIDF = GeneralQuery("hollow knight", '', 'windows', '', '', '', useTfIdf=False)
+rankAtributeTFIDF = dict(queryTFIDF.processQuery())
+print("Sem TFIDF: " + '.'.join(str(x) for x in queryTFIDF.processQuery()))
+
+# %%
+query = GeneralQuery("hollow knight", '', 'windows', '', '', '', useTfIdf=True)
+rankAtribute = dict(query.processQuery())
+print("Com TFIDF: " + '.'.join(str(x) for x in query.processQuery()))
+
+# %%
+sumSquareDistance = getSumSquareDist(rankAtributeTFIDF, rankAtribute)
+
+# %%
+numDocs = len(list(rankAtribute.keys())+list(rankAtributeTFIDF.keys()))
+spearmanCorrelation(sumSquareDistance, k = numDocs)
